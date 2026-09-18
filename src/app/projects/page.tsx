671795aaa361b9281d66eada8a3401fd2939dfe1
yiliey <yiliey@umich.edu>
2026-09-17 23:53:45 -0400

    deploy: publish PRISM portfolio site

--- src/app/projects/page.tsx
+++
@@ -1,5 +0,0 @@
-import FlowShowcase from "./FlowShowcase";
-
-export default function FlowConceptPage() {
-  return <FlowShowcase />;
-}
--- src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-interface FooterProps {
-  lastUpdated?: string;
-}
-
-export default function Footer({ lastUpdated }: FooterProps) {
-  return null;
-}
